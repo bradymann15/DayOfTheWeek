4174478ce5749512ad02d73fb00b887cb685ba36
Brady Mann <Football3320>
2019-04-02 13:57:12 -0400

    Made a void method for the introductory poem

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,6 +3,18 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
+    public static void poem(){
+        System.out.println("There is a nursery rhyme that goes like this....");
+        System.out.println(" ");
+        System.out.println("Monday's child is fair of face,");
+        System.out.println("Tuesday's child is full of grace,");
+        System.out.println("Wednesday's child is full of woe,");
+        System.out.println("Thursday's child has far to go.");
+        System.out.println("Friday's child is loving and giving,");
+        System.out.println("Saturday's child works hard for a living,");
+        System.out.println("But the child born on the Sabbath Day,");
+        System.out.println("Is fair and wise and good in every way.");
+        }
 }
