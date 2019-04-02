25fb5fd2abf2eb5cf548e960e8e91a271ec3746a
Brady Mann <Football3320>
2019-04-02 14:19:08 -0400

    Started on switch method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,12 +1,16 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
         poem();
         System.out.println(" ");
         System.out.println("So, which day of the week to you come from?");
-        System.out.println("What month were you born in?(Enter as number)");
+
     }
     public static void poem(){
         System.out.println("There is a nursery rhyme that goes like this....");
@@ -20,4 +24,22 @@ public class Main {
         System.out.println("But the child born on the Sabbath Day,");
         System.out.println("Is fair and wise and good in every way.");
         }
+    public static int birthmonth(Scanner keyboard){
+        System.out.println("What is your birth month?(Enter as number)");
+        int month = keyboard.nextInt();
+        switch(month){
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("Your month has 30 days");
+                break;
+            case 2:
+                System.out.println("February has 28 days, except on a leap year, where it has 29 days.");
+                break;
+                default:
+                    System.out.println("Your month ");
+
+        }
+    }
 }
