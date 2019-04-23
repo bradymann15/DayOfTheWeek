c384689ef2ceaf9a9fc061defaf81ca3d2ec8e96
Brady Mann <Football3320>
2019-04-23 14:03:10 -0400

    tried to implement a do while loop

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -10,8 +10,9 @@ public class Main {
         poem();
         System.out.println(" ");
         System.out.println("So, which day of the week to you come from?");
-        birthdateweekday(keyboard);
-
+        boolean value;
+        do {birthdateweekday(keyboard);
+        }while(value = true);
 
 
     }
@@ -29,88 +30,92 @@ public class Main {
         }
 
     public static void birthdateweekday(Scanner keyboard){
-        System.out.println("What is your birth month?");
-        String birth = keyboard.nextLine();
-        System.out.println("Now, enter your birth month as a number");
-        int month = keyboard.nextInt();
-        if (month == 1){
-            month = 13;
-        }
-        else if (month == 2){
-            month = 14;}
-        switch(month){
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                System.out.println(birth+" has 30 days in it");
-                break;
-            case 14:
-                System.out.println(birth+" has 28 days in it, and 29 days on a leap year");
-                break;
+            System.out.println("What is your birth month?");
+            String birth = keyboard.nextLine();
+            System.out.println("Now, enter your birth month as a number");
+            int month = keyboard.nextInt();
+            if (month == 1) {
+                month = 13;
+            } else if (month == 2) {
+                month = 14;
+            }
+            switch (month) {
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    System.out.println(birth + " has 30 days in it");
+                    break;
+                case 14:
+                    System.out.println(birth + " has 28 days in it, and 29 days on a leap year");
+                    break;
                 default:
-                    System.out.println(birth+" has 31 days in it");
+                    System.out.println(birth + " has 31 days in it");
                     break;
 
-        }
-        System.out.println("What is the date of your birth?");
-        int date = keyboard.nextInt();
-        System.out.println("What year were you born on?");
-        int year = keyboard.nextInt();
-        if (month == 13 || month == 14){
-            year = year-1;
-        }
-        int century = year/100;
-        int yearOfCentury = year%100;
-        String dayOfTheWeek;
-        int dayoftheweek =((date+((13*(month+1))/5)+yearOfCentury+(yearOfCentury/4)+(century/4)+(5*century))%7);
-        switch(dayoftheweek){
-            case 0:
-                dayOfTheWeek = "Saturday";
-                break;
-            case 1:
-                dayOfTheWeek = "Sunday";
-                break;
-            case 2:
-                dayOfTheWeek = "Monday";
-                break;
-            case 3:
-                dayOfTheWeek = "Tuesday";
-                break;
-            case 4:
-                dayOfTheWeek = "Wednesday";
-                break;
-            case 5:
-                dayOfTheWeek = "Thursday";
-                break;
-            default:
-                dayOfTheWeek = "Friday";
-                break;
-        }
-        System.out.println("You were born on a "+dayOfTheWeek);
-        switch(dayOfTheWeek){
-            case "Monday":
-                System.out.println("You are fair of face");
-                break;
-            case "Tuesday":
-                System.out.println("You are full of grace");
-                break;
-            case "Wednesday":
-                System.out.println("You are full of woe");
-                break;
-            case "Thursday":
-                System.out.println("You have far to go");
-                break;
-            case "Friday":
-                System.out.println("You are loving and giving");
-                break;
-            case "Saturday":
-                System.out.println("You work hard for a living");
-                break;
-            case "Sunday":
-                System.out.println("You are fair and wise and good in every way");
-                break;
-        }
+            }
+            System.out.println("What is the date of your birth?");
+            int date = keyboard.nextInt();
+            System.out.println("What year were you born on?");
+            int year = keyboard.nextInt();
+            if (month == 13 || month == 14) {
+                year = year - 1;
+            }
+            int century = year / 100;
+            int yearOfCentury = year % 100;
+            String dayOfTheWeek;
+            int dayoftheweek = ((date + ((13 * (month + 1)) / 5) + yearOfCentury + (yearOfCentury / 4) + (century / 4) + (5 * century)) % 7);
+            switch (dayoftheweek) {
+                case 0:
+                    dayOfTheWeek = "Saturday";
+                    break;
+                case 1:
+                    dayOfTheWeek = "Sunday";
+                    break;
+                case 2:
+                    dayOfTheWeek = "Monday";
+                    break;
+                case 3:
+                    dayOfTheWeek = "Tuesday";
+                    break;
+                case 4:
+                    dayOfTheWeek = "Wednesday";
+                    break;
+                case 5:
+                    dayOfTheWeek = "Thursday";
+                    break;
+                default:
+                    dayOfTheWeek = "Friday";
+                    break;
+            }
+            System.out.println("You were born on a " + dayOfTheWeek);
+            switch (dayOfTheWeek) {
+                case "Monday":
+                    System.out.println("You are fair of face");
+                    break;
+                case "Tuesday":
+                    System.out.println("You are full of grace");
+                    break;
+                case "Wednesday":
+                    System.out.println("You are full of woe");
+                    break;
+                case "Thursday":
+                    System.out.println("You have far to go");
+                    break;
+                case "Friday":
+                    System.out.println("You are loving and giving");
+                    break;
+                case "Saturday":
+                    System.out.println("You work hard for a living");
+                    break;
+                case "Sunday":
+                    System.out.println("You are fair and wise and good in every way");
+                    break;
+                    }
+            System.out.println("Would you like to try out a different date?");
+            }
+          
+
         
 
 
